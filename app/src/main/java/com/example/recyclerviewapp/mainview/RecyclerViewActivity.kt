704cecf196f494533c276fd2b894766ebf4c5237
Lautaro-io <chelo43360@gmail.com>
2024-11-26 14:57:27 -0300

    finish-prac

--- app/src/main/java/com/example/recyclerviewapp/mainview/RecyclerViewActivity.kt
+++ app/src/main/java/com/example/recyclerviewapp/mainview/RecyclerViewActivity.kt
@@ -1,8 +1,10 @@
 package com.example.recyclerviewapp.mainview
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recyclerviewapp.R
@@ -15,9 +17,21 @@ class RecyclerViewActivity : AppCompatActivity() {
         setContentView(R.layout.activity_recycler_view)
         initRv()
     }
-    private fun initRv(){
-        val recyclerView = findViewById<RecyclerView>(R.id.rSuperHero )
+
+    private fun initRv() {
+        val manager = LinearLayoutManager(this)
+        val recyclerView = findViewById<RecyclerView>(R.id.rSuperHero)
+        val decoration = DividerItemDecoration(this,manager.orientation)
         recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = SuperHeroAdapter(SuperHeroProvider.superHeroList)
+        recyclerView.adapter = SuperHeroAdapter(SuperHeroProvider.superHeroList) { superhero ->
+            onItemSelected(
+                superhero
+            )
+        }
+        recyclerView.addItemDecoration(decoration)
+    }
+
+    fun onItemSelected(superHero: SuperHero) {
+        Toast.makeText(this,superHero.superhero,Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/example/recyclerviewapp/mainview/adapter/SuperHeroViewHolder.kt
+++ app/src/main/java/com/example/recyclerviewapp/mainview/adapter/SuperHeroViewHolder.kt
@@ -1,26 +1,31 @@
 package com.example.recyclerviewapp.mainview.adapter
+
 import com.example.recyclerviewapp.R
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.recyclerviewapp.mainview.SuperHero
 import com.example.recyclerviewapp.mainview.SuperHeroProvider.Companion.superHeroList
 
-class SuperHeroViewHolder(view: View): RecyclerView.ViewHolder(view){
+class SuperHeroViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     val superHero = view.findViewById<TextView>(R.id.spName)
     val realName = view.findViewById<TextView>(R.id.spRealName)
     val publisher = view.findViewById<TextView>(R.id.tvPublisher)
     val photo = view.findViewById<ImageView>(R.id.imgViewSP)
 
-    fun render(superHeroModel : SuperHero){
+    fun render(superHeroModel: SuperHero, onClickListener: (SuperHero) -> Unit) {
         superHero.text = superHeroModel.superhero
         realName.text = superHeroModel.realNane
         publisher.text = superHeroModel.publisher
+        Glide.with(photo.context).load(superHeroModel.photo).into(photo)
 
-
-
+        itemView.setOnClickListener {
+            onClickListener(superHeroModel)
+        }
     }
 }
